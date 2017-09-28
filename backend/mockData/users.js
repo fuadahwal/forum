681c72f36a917eeee54f830188697b3305044330
Fuad Ahwal <cerdsalicious@Fuads-MacBook-Pro.local>
2017-09-28 13:30:37 -0700

    first commit

---
+++ backend/mockData/users.js
@@ -0,0 +1,39 @@
+const users = [
+  {
+    'user_id': 0,
+    'username': 'testuser1',
+    'email': 'testuser1@leforum.rocks',
+    'avatarUrl': 'https://robohash.org/quisapientelibero.png?size=50x50&set=set1',
+    'name': 'Test Usee 1',
+  },
+  {
+    'user_id': 1,
+    'username': 'testuser2',
+    'email': 'testuser2@leforum.rocks',
+    'avatarUrl': 'https://robohash.org/magnidictadeserunt.png?size=50x50&set=set1',
+    'name': 'Test Usee 2',
+  },
+  {
+    'user_id': 2,
+    'username': 'testuser3',
+    'email': 'testuser3@leforum.rocks',
+    'avatarUrl': 'https://robohash.org/ducimusnostrumillo.jpg?size=50x50&set=set1',
+    'name': 'Test Usee 3',
+  },
+  {
+    'user_id': 3,
+    'username': 'testuser4',
+    'email': 'testuser4@leforum.rocks',
+    'avatarUrl': 'https://robohash.org/autemharumvitae.bmp?size=50x50&set=set1',
+    'name': 'Test Usee 4',
+  },
+  {
+    'user_id': 4,
+    'username': 'testuser5',
+    'email': 'testuser5@leforum.rocks',
+    'avatarUrl': 'https://robohash.org/similiquealiquidmaiores.jpg?size=50x50&set=set1',
+    'name': 'Test Usee 5',
+  },
+];
+
+module.exports = users;
